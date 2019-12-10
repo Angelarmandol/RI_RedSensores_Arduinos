ee568da76901e313b005a772737745ea63d7a221
Angel Lopez <angelarmandol@hotmail.com>
2019-12-10 13:02:01 -0800

    code iteration

--- MainNode/NodeMcu/NodeMcu.ino
+++ MainNode/NodeMcu/NodeMcu.ino
@@ -1,6 +1,3 @@
-#include <Pinger.h>
-#include <PingerResponse.h>
-
 #include <BearSSLHelpers.h>
 #include <CertStoreBearSSL.h>
 #include <ESP8266WiFi.h>
@@ -29,29 +26,25 @@
 const char* ssid = "Fam Lopez";
 const char* password = "";
 String page = "";
+String header;
+
+
 int LEDPin = 0;
 int LEDPin1 = 1;
 int LEDPin2 = 2;
 int LEDPin3 = 3;
 int LEDPin4 = 4;
  
-
 ESP8266WebServer server(80); 
-String header;
-// Current time
+
 unsigned long currentTime = millis();
-// Previous time
 unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
-// Client Ip Address……
 
 
 
 IPAddress staticIP(192,168,1,22);
-
 IPAddress gateway(192,168,1,254);
-
 IPAddress subnet(255,255,255,0);
 
 // Access point Ip Address……
@@ -66,46 +59,70 @@ void setup(void)
 
 {
 
+   pinMode(LEDPin1, OUTPUT);
+   pinMode(LEDPin2, OUTPUT);
+   pinMode(LEDPin3, OUTPUT);
+   pinMode(LEDPin4, OUTPUT);
+   
    digitalWrite(LEDPin1, LOW);
    digitalWrite(LEDPin2, LOW);
    digitalWrite(LEDPin3, LOW);
    digitalWrite(LEDPin4, LOW);
 
+   
   if (!SD.begin(CS_PIN)) {
     Serial.println("Falla SD.");
-    //programa encerrrado
     return;
   }
    
-  //se chegou aqui é porque o cartão foi inicializado corretamente  
   Serial.println("SD OK.");
  
- 
 
- 
+        delay(1000);
+        Serial.begin(115200);
+        WiFi.begin(ssid, password); //begin WiFi connection
+        Serial.println("");
+       
+        // Wait for connection
+        while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+        }
+        Serial.println("");
+        Serial.print("Connected to ");
+        Serial.println(ssid);
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
 
+
+              /////Soft AP
+              Serial.printf("Connecting to %s\n", ssid);      
+              WiFi.begin(ssid, password);
+              WiFi.config(staticIP, gateway, subnet);
+              
+              while (WiFi.status() != WL_CONNECTED){
+              delay(500);
+              Serial.print(".");
+              }
+              
+              Serial.println();
+              Serial.print("Connected, IP address — Client Ip: ");
+              Serial.println(WiFi.localIP());
+              Serial.println();
+              Serial.print("Setting soft-AP configuration … ");
+              Serial.println(WiFi.softAPConfig(local_IP, gateway2, subnet2) ? "Ready" : "Failed!");
+              Serial.print("Setting soft-AP … ");
+              Serial.println(WiFi.softAP("Principal") ? "Ready" : "Failed!");
+              Serial.print("Soft-AP IP address = ");
+              Serial.println(WiFi.softAPIP());
+      
+
+
+        
    
   //the HTML of the web page
   page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p><a href=\"Send\"><button>Send</button></a>";
-  //make the LED pin output and initially turned off
-  pinMode(LEDPin, OUTPUT);
-  digitalWrite(LEDPin, LOW);
-   
-  delay(1000);
-  Serial.begin(115200);
-  WiFi.begin(ssid, password); //begin WiFi connection
-  Serial.println("");
- 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+
    
   server.on("/", [](){
     server.send(200, "text/html", page);
@@ -142,43 +159,8 @@ void setup(void)
   Serial.println("Web server started!");
 
   
-Serial.printf("Connecting to %s\n", ssid);
-
-WiFi.begin(ssid, password);
-WiFi.config(staticIP, gateway, subnet);
-while (WiFi.status() != WL_CONNECTED)
-
-{
-
-delay(500);
-
-Serial.print(".");
-
-}
-
-Serial.println();
-
-Serial.print("Connected, IP address — Client Ip: ");
-
-Serial.println(WiFi.localIP());
-
-////// Connection to the Access point……
-Serial.println();
-
-Serial.print("Setting soft-AP configuration … ");
-
-Serial.println(WiFi.softAPConfig(local_IP, gateway2, subnet2) ? "Ready" : "Failed!");
-
-Serial.print("Setting soft-AP … ");
-
-Serial.println(WiFi.softAP("Principal") ? "Ready" : "Failed!");
-
-Serial.print("Soft-AP IP address = ");
-
-Serial.println(WiFi.softAPIP());
-
-}
+        
+}// end of setup
 void loop() {
     server.handleClient();
-   
-  }
+    }
